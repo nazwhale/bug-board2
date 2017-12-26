cd1a0e3685b55b59c786261891a7536a514d5482
Naz Malik <naz@Nazs-MBP.lan>
2017-12-26 17:48:09 +0000

    Set up react redux thunk

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyCKytAJOozuPk5zLsDWTnwZzxGxsR5XB1s",
+  authDomain: "bugs-board.firebaseapp.com",
+  databaseURL: "https://bugs-board.firebaseio.com",
+  projectId: "bugs-board",
+  storageBucket: "bugs-board.appspot.com",
+  messagingSenderId: "264359786595"
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database.ref('posts/');
