e710012d7dd424c7bbcad3b54dfba87498961564
Naz Malik <naz@Nazs-MacBook-Pro.local>
2018-01-07 23:56:45 +0000

    Extract Card component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Field, reduxForm, reset } from 'redux-form';
-import Modal from 'react-modal';
-import _ from 'lodash';
-import { getPosts, savePost, deletePost, setCompleted, setIncomplete, upvote } from './actions/post_actions';
 import './styles/app.css'
+import { Field, reduxForm }  from 'redux-form';
+import _ from 'lodash';
+import { getPosts, savePost } from './actions/post_actions';
 import SearchBar from './containers/search_bar';
-
-const modalStyle = {
-  overlay : {
-    position          : 'fixed',
-    top               : 300,
-    left              : 500,
-    right             : 500,
-    bottom            : 100,
-    backgroundColor   : 'transparent',
-    zIndex            : 99
-  },
-  content : {
-    position                   : 'absolute',
-    top                        : '40px',
-    left                       : '40px',
-    right                      : '40px',
-    bottom                     : '40px',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px',
-  }
-}
+import Card from './containers/card';
 
 class App extends Component {
-
-  constructor() {
-    super();
-
-    this.state = { modalOpen : false, modalKey: undefined, modalVotes: undefined }
-  }
 
   componentWillMount() {
     this.props.getPosts();
@@ -58,26 +26,6 @@ class App extends Component {
     this.props.savePost(values);
   }
 
-  changeStatus(post, key) {
-    if (!post.completed) {
-      this.props.setCompleted(key);
-    } else {
-      this.props.setIncomplete(key);
-    };
-  }
-
-  openModal(key, votes) {
-    this.setState({ modalOpen: true, modalKey: key, modalVotes: votes })
-  }
-
-  closeModal() {
-    this.setState({ modalOpen: false })
-  }
-
-  setDoneColor(post) {
-    return post.completed ? 'btn btn-done' : 'btn btn-incomplete';
-  }
-
   // filterSearchResults(searchTerm) {
   //   _.map(this.props.posts, (post) => {
   //     if
@@ -89,45 +37,18 @@ class App extends Component {
 
     return _.map(this.props.posts, (post, key) => {
       return (
-        // extract into card container
-        <div className='card post post-item' key={key}>
-          <div className='card block'>
-            <h3 className='card-title'>{post.title}</h3>
-
-            <h3>{ post.votes }</h3>
-
-            <a className='card-text'>{post.body}</a>
-
-            <button className={this.setDoneColor(post)} onClick={() => {
-              this.changeStatus(post, key);
-            }}>
-              Done
-            </button>
-
-            <button className='btn btn-danger' onClick={() => {
-              this.props.deletePost(key);
-            }}>
-              X
-            </button>
-
-            <button className='btn btn-upvote' onClick={() => {
-              this.openModal(key, post.votes);
-            }}>
-              ^
-            </button>
-
-          </div>
+        <div>
+          <Card post={post} dupeKey={key} />
         </div>
       );
     });
   }
 
-
   render() {
     const { handleSubmit } = this.props;
+    // extract input box into container
     return (
       <div className='container'>
-        // extract input box into container
         <div className='navbar'>
           <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
             <Field
@@ -148,29 +69,9 @@ class App extends Component {
 
         <SearchBar />
 
-
         <div className='main'>
           { this.renderPosts() }
         </div>
-
-        <Modal isOpen={ this.state.modalOpen } style={ modalStyle } >
-          <h1>Modal Content</h1>
-          <p>Etc.</p>
-          <button className='btn btn-upvote' onClick={() => {
-            this.closeModal();
-          }}>
-          close
-          </button>
-
-         <button className='btn btn-upvote' onClick={() => {
-           this.props.upvote( this.state.modalKey, this.state.modalVotes );
-           this.closeModal();
-          }}>
-           ^
-         </button>
-
-
-        </Modal>
 
       </div>
     );
@@ -186,7 +87,7 @@ function mapStateToProps({ posts, searchTerm }) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ getPosts, savePost, deletePost, setCompleted, setIncomplete, upvote }, dispatch);
+  return bindActionCreators({ getPosts, savePost }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(form);
---
+++ src/containers/card.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import '../styles/app.css'
+import Modal from 'react-modal';
+import { setCompleted, setIncomplete, deletePost, upvote } from '../actions/post_actions';
+
+const modalStyle = {
+  overlay : {
+    position          : 'fixed',
+    top               : 300,
+    left              : 500,
+    right             : 500,
+    bottom            : 100,
+    backgroundColor   : 'transparent',
+    zIndex            : 99
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    left                       : '40px',
+    right                      : '40px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px',
+  }
+}
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen : false, modalKey: undefined, modalVotes: undefined }
+  }
+
+  changeStatus(post, key) {
+    if (!this.props.post.completed) {
+      this.props.setCompleted(key);
+    } else {
+      this.props.setIncomplete(key);
+    };
+  }
+
+  setDoneColor(post) {
+    return this.props.post.completed ? 'btn btn-done' : 'btn btn-incomplete';
+  }
+
+  openModal(key, votes) {
+    this.setState({ modalOpen: true, modalKey: key, modalVotes: votes })
+  }
+
+  closeModal() {
+    this.setState({ modalOpen: false })
+  }
+
+  render() {
+
+    return (
+      <div className='card post post-item' key={ this.props.post.key }>
+        <div className='card block'>
+          <h3 className='card-title'>{ this.props.post.title }</h3>
+
+          <h3>{ this.props.post.votes }</h3>
+
+          <a className='card-text'>{ this.props.post.body }</a>
+
+          <button className={this.setDoneColor(this.props.post)} onClick={() => {
+            this.changeStatus(this.props.post, this.props.dupeKey);
+          }}>
+            Done
+          </button>
+
+          <button className='btn btn-danger' onClick={() => {
+            this.props.deletePost(this.props.dupeKey);
+          }}>
+            X
+          </button>
+
+          <button className='btn btn-upvote' onClick={() => {
+            this.openModal(this.props.dupeKey, this.props.post.votes);
+          }}>
+            ^
+          </button>
+
+        </div>
+
+        <Modal isOpen={ this.state.modalOpen } style={ modalStyle } >
+          <h1>Upvote?</h1>
+          <p>Enter UserID here</p>
+          <button className='btn btn-upvote' onClick={() => {
+            this.closeModal();
+          }}>
+          close
+          </button>
+
+         <button className='btn btn-upvote' onClick={() => {
+           this.props.upvote( this.state.modalKey, this.state.modalVotes );
+           this.closeModal();
+          }}>
+           Confirm
+         </button>
+
+        </Modal>
+
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ setCompleted, setIncomplete, deletePost, upvote }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(Card);
